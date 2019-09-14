0ceb4558c9ff3b79df9e4cf0afa8192a26d9f206
remorses <beats.by.morse@gmail.com>
2019-09-14 15:04:09 +0200

    tartiflette_scalars in another module

--- module/scalars.py
+++ module/scalars.py
@@ -1,21 +1,6 @@
 
 from tartiflette import Scalar
-from bson import ObjectId
-from typing import Union
-from tartiflette.language.ast.base import Node
-from tartiflette.constants import UNDEFINED_VALUE
-
+from tartiflette_scalars import Json, ObjectId
 
 JsonScalar = Scalar("Json")
-@JsonScalar
-class JsonClass:
-    @staticmethod
-    def coerce_input(val):
-        return val
-
-    @staticmethod
-    def coerce_output(val):
-        return val
-
-    def parse_literal(self, ast: "Node") -> Union[str, "UNDEFINED_VALUE"]:
-        return self.coerce_input(ast.value)
+JsonScalar(Json)
